6982ee12ce0ab02778029c08fccc9e60115bc2c3
Marco Michele Accorinti <marco.accorinti@itsincom.it>
2026-04-08 15:54:20 +0200

    Creazione CRUD base StatusController

--- MangaRepositoryAPI/Controllers/GenreController.cs
+++ MangaRepositoryAPI/Controllers/GenreController.cs
@@ -16,6 +16,11 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Get
 
+        /// <summary>
+        /// Retrieves all genres as data transfer objects.
+        /// </summary>
+        /// <returns>An <see cref="IActionResult"/> containing a collection of genre DTOs with HTTP 200 (OK) if successful;
+        /// otherwise, an error response with the appropriate HTTP status code.</returns>
         [HttpGet]
         public IActionResult GetAll() 
         {
@@ -31,6 +36,14 @@ namespace MangaRepositoryAPI.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves the details of a genre by its unique identifier.
+        /// </summary>
+        /// <remarks>Returns a 500 Internal Server Error if an unexpected error occurs during
+        /// processing.</remarks>
+        /// <param name="id">The unique identifier of the genre to retrieve.</param>
+        /// <returns>An IActionResult containing the detailed genre data if found; otherwise, a NotFound result if the genre does
+        /// not exist.</returns>
         [HttpGet]
         [Route("{id}")]
         public IActionResult GetById([FromRoute] Guid id) 
@@ -57,6 +70,14 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Post
 
+
+        /// <summary>
+        /// Creates a new genre using the provided data transfer object.
+        /// </summary>
+        /// <param name="dto">The data transfer object containing the information required to create a new genre. Must not be null.</param>
+        /// <returns>An IActionResult indicating the result of the create operation. Returns 200 OK if the genre is created
+        /// successfully; 422 Unprocessable Entity if the creation fails; or 500 Internal Server Error if an unexpected
+        /// error occurs.</returns>
         [HttpPost]
         public IActionResult Create([FromBody] GenreDTO dto) 
         {
@@ -88,6 +109,16 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Put
 
+        /// <summary>
+        /// Updates the details of an existing genre with the specified identifier.
+        /// </summary>
+        /// <remarks>The request must include a valid genre identifier and a non-null GenreDTO in the
+        /// request body. The operation updates only the genre's name. If the genre does not exist, the method returns
+        /// 422 Unprocessable Entity.</remarks>
+        /// <param name="dto">The data transfer object containing the updated genre information. Cannot be null.</param>
+        /// <param name="id">The unique identifier of the genre to update.</param>
+        /// <returns>An IActionResult indicating the result of the update operation. Returns 200 OK if the update is successful,
+        /// 422 Unprocessable Entity if the update fails, or 500 Internal Server Error if an unexpected error occurs.</returns>
         [HttpPut]
         public IActionResult Update([FromBody] GenreDTO dto, [FromRoute] Guid id) 
         {
@@ -117,6 +148,13 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Delete
 
+        /// <summary>
+        /// Deletes the genre with the specified identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the genre to delete.</param>
+        /// <returns>An <see cref="OkResult"/> if the genre was successfully deleted; a <see cref="NotFoundResult"/> if the genre
+        /// does not exist; an <see cref="UnprocessableEntityResult"/> if the deletion could not be completed; or a <see
+        /// cref="ObjectResult"/> with status code 500 if an unexpected error occurs.</returns>
         [HttpDelete]
         public IActionResult Delete([FromRoute] Guid id) 
         {
--- MangaRepositoryAPI/Controllers/StatusController.cs
+++ MangaRepositoryAPI/Controllers/StatusController.cs
@@ -1,7 +1,9 @@
 ﻿using MangaRepositoryAPI.Data;
 using MangaRepositoryAPI.DTO;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MangaRepositoryAPI.Controllers
 {
@@ -15,12 +17,20 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Get
 
+        /// <summary>
+        /// Retrieves all status records as data transfer objects (DTOs).
+        /// </summary>
+        /// <remarks>If an internal server error occurs, the response will have HTTP status code 500 and
+        /// include error details. The returned collection will be empty if no status records are found.</remarks>
+        /// <returns>An <see cref="IActionResult"/> containing a collection of status DTOs with HTTP 200 (OK) if successful;
+        /// otherwise, an error response with the appropriate HTTP status code.</returns>
         [HttpGet]
         public IActionResult GetAll() 
         {
             try
             {
-
+                var result = _ctx.Statuses.ToList().ConvertAll(_mapper.MapEntityToDTO);
+                return Ok(result);
             }
             catch (Exception ex)
             {
@@ -29,13 +39,28 @@ namespace MangaRepositoryAPI.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves the status entity with the specified unique identifier.
+        /// </summary>
+        /// <remarks>The returned status entity includes related manga information. This endpoint is
+        /// typically used to fetch a single status by its identifier for display or further processing.</remarks>
+        /// <param name="id">The unique identifier of the status to retrieve.</param>
+        /// <returns>An <see cref="IActionResult"/> containing the status entity if found; otherwise, a 404 Not Found result.
+        /// Returns a 500 Internal Server Error result if an unexpected error occurs.</returns>
         [HttpGet]
         [Route("{id}")]
         public IActionResult GetById([FromRoute] Guid id) 
         {
             try
             {
-
+                var result = _ctx.Statuses
+                                 .Include(s => s.Manga)
+                                 .SingleOrDefault(s => s.StatusId == id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
             }
             catch (Exception ex)
             {
@@ -48,12 +73,35 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Post
 
+        /// <summary>
+        /// Creates a new status entry using the provided data and returns the created status.
+        /// </summary>
+        /// <remarks>Returns a 201 response with the created status if the operation succeeds. Returns a
+        /// 422 response if the status could not be created, or a 500 response if an unexpected error occurs.</remarks>
+        /// <param name="dto">The data transfer object containing the information required to create a new status. Cannot be null.</param>
+        /// <returns>An IActionResult containing the created status if successful; otherwise, an error response indicating the
+        /// reason for failure.</returns>
         [HttpPost]
         public IActionResult Create([FromBody] StatusDTO dto) 
         {
             try
             {
-
+                var result = new Status()
+                {
+                    StatusId = Guid.NewGuid(),
+                    Description = dto.Description,
+                    MangaId = dto.MangaId,
+                    Manga = _ctx.Mangas.SingleOrDefault(m => m.MangaId == dto.MangaId)
+                };
+                _ctx.Statuses.Add(result);
+                if (_ctx.SaveChanges() > 0)
+                {
+                    return  Ok(_mapper.MapEntityToDTO(result));
+                }
+                else
+                {
+                    return UnprocessableEntity();
+                }
             }
             catch (Exception ex)
             {
@@ -66,12 +114,33 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Put
 
+        /// <summary>
+        /// Updates the status entity with the specified identifier using the provided data transfer object.
+        /// </summary>
+        /// <remarks>This method requires a valid status identifier and a non-null data transfer object.
+        /// The update is applied to the status entity matching the provided identifier.</remarks>
+        /// <param name="dto">The data transfer object containing the updated status information. Cannot be null.</param>
+        /// <param name="id">The unique identifier of the status entity to update.</param>
+        /// <returns>An IActionResult indicating the result of the update operation. Returns 200 OK if the update is successful,
+        /// 422 Unprocessable Entity if the update fails, or 500 Internal Server Error if an unexpected error occurs.</returns>
         [HttpPut]
         public IActionResult Update([FromBody] StatusDTO dto,[FromRoute] Guid id) 
         {
             try
             {
-
+                var result = _ctx.Statuses
+                                 .Include(s => s.Manga)
+                                 .SingleOrDefault(s => s.StatusId == id);
+                result.Description = dto.Description;
+                result.MangaId = dto.MangaId;
+                if (_ctx.SaveChanges() > 0) 
+                {
+                    return Ok();
+                } 
+                else 
+                {
+                    return UnprocessableEntity();
+                }
             }
             catch (Exception ex)
             {
@@ -84,12 +153,38 @@ namespace MangaRepositoryAPI.Controllers
 
         #region Delete
 
+        /// <summary>
+        /// Deletes the status entity with the specified identifier.
+        /// </summary>
+        /// <remarks>This action requires a valid status identifier. If the specified status is not found,
+        /// a 404 Not Found response is returned. If the deletion fails due to a processing error, a 422 Unprocessable
+        /// Entity response is returned. Any unexpected errors result in a 500 Internal Server Error response.</remarks>
+        /// <param name="id">The unique identifier of the status to delete.</param>
+        /// <returns>An <see cref="IActionResult"/> indicating the result of the delete operation. Returns <see cref="OkResult"/>
+        /// if the status was deleted successfully, <see cref="NotFoundResult"/> if the status does not exist, <see
+        /// cref="UnprocessableEntityResult"/> if the deletion could not be completed, or a server error result if an
+        /// exception occurs.</returns>
         [HttpDelete]
         public IActionResult Delete([FromRoute] Guid id) 
         {
             try
             {
-
+                var result = _ctx.Statuses
+                                 .Include(s => s.Manga)
+                                 .SingleOrDefault(s => s.StatusId == id);
+                if (result == null) 
+                {
+                    return NotFound();
+                }
+                _ctx.Statuses.Remove(result);
+                if (_ctx.SaveChanges() > 0)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return UnprocessableEntity();
+                }
             }
             catch (Exception ex)
             {
